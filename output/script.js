ebc1ef27921b9f0d304c21863db8b812f0448772
omar-faruk02 <tuhin.omar99@gmail.com>
2021-12-28 16:47:52 +0600

    object oriented ts

--- output/script.js
+++ output/script.js
@@ -24,3 +24,55 @@ const object = {
 };
 UserDetails(12, object);
 // sfignature function
+// return void
+let UserInfo;
+UserInfo = (id, user) => {
+    return;
+};
+//return number
+let Calculation;
+Calculation = (a, b, c) => {
+    if (c == "devide") {
+        return a / b;
+    }
+    else {
+        return a * b;
+    }
+};
+const result = Calculation(4, 2, "devide");
+console.log(result);
+// class & object
+class Player {
+    constructor(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+    play() {
+        return `the name of the palyer is ${this.name} age is ${this.age} and from ${this.country} `;
+    }
+}
+class Club extends Player {
+    constructor(c, name, age, country) {
+        super(name, age, country);
+        this.club = c;
+    }
+    setPlayer() {
+        return `${this.play()} and he also play in ${this.club}`;
+    }
+}
+const messi = new Club("Barcelona", "messi", 33, "argentina");
+console.log(messi.setPlayer());
+// access modifier
+class Developer {
+    constructor(designation, language, salary) {
+        this.designation = designation;
+        this.language = language;
+        this.salary = salary;
+    }
+    All() {
+        return `Mr. Rakib is ${this.designation} he is in expert in ${this.language} he withdraw $${this.salary} every month`;
+    }
+}
+const rakib = new Developer("Developer", "React", 1200);
+console.log(rakib.All());
--- src/script.ts
+++ src/script.ts
@@ -37,4 +37,70 @@ const object={
 UserDetails(12, object)
 
 // sfignature function
+// return void
+let UserInfo:(id:UserId,user:UserType)=> void;
 
+UserInfo=(id:UserId,user:UserType)=>{
+    return 
+}
+//return number
+
+let Calculation:(a:number,b:number,c:string)=>number
+
+Calculation=(a:number,b:number,c:string)=>{
+    if (c=="devide") {
+        return a/b
+    } else {
+        return a*b
+    }
+}
+const result=Calculation(4,2,"devide")
+console.log(result)
+
+// class & object
+
+class Player{
+    name: string;
+    age: number;
+    country:string
+    constructor(name:string,age:number,country:string){
+        this.name=name
+        this.age=age
+        this.country=country
+
+    }
+    play(){
+       return `the name of the palyer is ${this.name} age is ${this.age} and from ${this.country} `
+    }
+}
+
+class Club extends Player{
+    club:string
+    constructor(c:string,name:string,age:number,country:string){
+        super(name,age,country)
+        this.club=c
+
+    }
+    setPlayer(){
+        return `${this.play()} and he also play in ${this.club}`
+    }
+}
+
+const messi= new Club("Barcelona","messi",33,"argentina")
+console.log(messi.setPlayer())
+
+// access modifier
+
+class Developer{
+    constructor(
+        public designation: string,
+        readonly language:string,
+        private salary:number
+    ){}
+    All(){
+        return `Mr. Rakib is ${this.designation} he is in expert in ${this.language} he withdraw $${this.salary} every month`
+    }
+}
+const rakib=new Developer("Developer","React",1200)
+
+console.log(rakib.All())
